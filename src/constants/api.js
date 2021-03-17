16449ca924de0c793604d70bbdf37aaea4125cd8
Grace Teng <pelicularities@users.noreply.github.com>
2021-03-17 17:16:17 +0800

    Update API URL to production API

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,4 @@
-// const REACT_APP_API_URL = "https://express-musicards-test.herokuapp.com";
-const REACT_APP_API_URL = "http://localhost:3000";
+const REACT_APP_API_URL = "https://express-musicards-test.herokuapp.com";
+// const REACT_APP_API_URL = "http://localhost:3000";
 
 export { REACT_APP_API_URL };
